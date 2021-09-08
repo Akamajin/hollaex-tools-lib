b87222abb74b5a4b6c6e1a5e97fa1a4179592f6d
akamajin <shahab@dotest.ir>
2021-09-08 19:53:12 +0430

    new functions

---
+++ tools/balances.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { getModel } = require('./database/model');
+
+const createBalanceRow = ({email,action,amount,interest_rate,created_at}) => {
+	return getModel('sequelize').transaction((transaction) => {
+		return dbQuery.findOne('user', {
+			where: { email }
+		})
+		.then((user) => {
+			return getModel('user').create({
+				user_id: user.id,
+				created_at,
+				interest_rate,
+				action,
+				amount
+			});
+		})
+	});
+};
+
+const updateBalanceRow = ({id,action,amount,interest_rate,created_at}) => {
+	return dbQuery.findOne('balances', { where: { id: id } })
+	.then((balance) => balance.update(
+		{action,amount,interest_rate,created_at},
+		{ fields: ['action','amount','interest_rate','created_at'] })
+	);
+};
+
+const deleteBalanceRow = ({id}) => {
+	return getModel('balances').destroy({ id });
+};
+
+const getBalanceRowByUserId = (userId) => {
+	return dbQuery.findAndCountAllWithRows('balances', {
+		where: {
+			user_id: userId,
+		},
+		attributes: {
+			exclude: ['user_id', 'updated_at']
+		},
+		order: [['created_at', 'DESC'], ['id', 'ASC']],
+	}).then(({ count, data }) => {count, data});
+};
+
+
+module.exports = {
+	createBalanceRow,
+	updateBalanceRow,
+	deleteBalanceRow,
+	getBalanceRowByUserId
+};
